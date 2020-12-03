2a89f2e30788e5200c90a6ee494524003a52338b
Joshua Davis <jhdavis@udel.edu>
2020-12-02 22:53:50 -0500

    Basic structure of lockdown model done. Needs debugging

--- lockdown-model.py
+++ lockdown-model.py
@@ -5,6 +5,8 @@ import random
 
 def overall_exposure(p_values):
     p_values = list(map(float, p_values))
+    if len(p_values) == 1:
+        return p_values[0]
     overall_prob = sum(p_values)
     for i in range(1, len(p_values)):
         combos = list(combinations(p_values, i + 1))
@@ -17,42 +19,89 @@ class Household:
     exposure_chance = 0
     risk_factor = 0
     
-    def __init__(self, s, e, r, c):
+    def __init__(self, s, e, r):
         self.social_eagerness = s
         self.exposure_chance = e
         self.risk_factor = r
-        self.coalition_id = c
 
     def value(self):
-        return 1/(self.social_eagerness * self.exposure)
+        return self.risk_factor/(self.social_eagerness * self.exposure)
 
-    def coalition_payoff(self, members):
-        exposures_list = [o.exposure_chance for o in members.append(self)]
-        coalition_exposure = overall_exposure(exposures_list)
-        return pow(self.social_eagerness, 1 - risk_factor) / pow(coalition_exposure, risk_factor)
-
-    def set_preferred_coaliton(self, coalition_set):
-        max_coalition_payoff = -1
-        for a in agent_set:
-            payoff = 0 # Need to finish this function
-        return None
+    def coalition_payoff(self, coalition):
+        members = coalition.members
+        if len(members) == 1 and members[0] == self:
+            return self.value()
+        else:
+            exposures_list = [o.exposure_chance for o in list(set().union(members, [self]))]
+            coalition_exposure = overall_exposure(exposures_list)
+            return pow(self.social_eagerness, 1 - risk_factor) / pow(coalition_exposure, risk_factor)
 
 class Coalition:
     members = []
+    idnum = -1
 
-    def __init__(self, m):
+    def __init__(self, m, i):
         self.members = m
-
-    def merge(self, other):
-        self.members += other.members
-
+        self.idnum = i
+    
 class World:
     agent_set = []
     coalition_set = []
 
-    def __init__(self, a, c):
-        self.agent_set = a
-        self.coalition_set = c
+    def __init__(self):
+        self.agent_set = []
+        for i in range(0, NUM_AGENTS):
+            self.agent_set.append(Household(random.uniform(MIN_SOC,  MAX_SOC),
+                                            random.uniform(MIN_EX,   MAX_EX),
+                                            random.uniform(MIN_RISK, MAX_RISK)))
+        self.coalition_set = []
+        for a in self.agent_set:
+            self.coalition_set.append(Coalition([a], i))
+
+    def move_to(self, agent, coalition_id):
+        for c in self.coalition_set:
+            if c.idnum == coalition_id:
+                if agent not in c.members:
+                    c.members.append(agent)
+            elif agent in c.members:
+                c.members.remove(agent)
+        return None
+
+    def current_coalition(self, agent):
+        for c in coalition_set:
+            if agent in c.members:
+                return c
+        return None
+    
+    def best_coalition(self, agent):
+        active_coalitions = []
+        for c in self.coalition_set:
+            if c.members:
+                active_coalitions.append(c)
+        best_coalition_id = -1
+        max_payoff = -1
+        for c in active_coalitions:
+            cur_payoff = agent.coalition_payoff(c)
+            if cur_payoff > max_payoff:
+                best_coalition_id = c.idnum
+                max_payoff = cur_payoff
+        return best_coalition_id
+                
+    def simulate(self):
+        for i in range(0, MAX_TIMESTEPS):
+            # simulation main loop
+            random.shuffle(self.agent_set)
+            for a in self.agent_set:
+                best = self.best_coalition(a)
+                cur = self.current_coalition(a)
+                if best != cur:
+                    self.move_to(a, best)
+                # determine coalition with the highest value
+                # compare highest value to self.
+                # if self is best, leave coalition/stay alone
+                # if a coalition is best, join it/stay if already there
+            if i % 15 == 0:
+                print(self.coalition_set)
         
 NUM_AGENTS = 20
 MAX_TIMESTEPS = 500
@@ -62,17 +111,11 @@ MAX_SOC = 1
 MIN_EX = 0.01
 MAX_EX = 0.10
 MIN_RISK = 0
-MAX_RISK = 1
+MAX_RISK = 1    
+    
+assert overall_exposure([0.5, 0.2]) == 0.6
+assert overall_exposure([0.5, 0.2, 0.3]) == 0.72
 
-agent_set = []
-for i in range(0, NUM_AGENTS):
-    agent_set.append(Household(random.uniform(MIN_SOC,  MAX_SOC),
-                               random.uniform(MIN_EX,   MAX_EX),
-                               random.uniform(MIN_RISK, MAX_RISK)))
+world_1 = World()
+world_1.simulate()
 
-    
-for i in range(0, MAX_TIMESTEPS):
-    
-    
-assert overall_exposure(0.5, 0.2) == 0.6
-assert overall_exposure(0.5, 0.2, 0.3) == 0.72
